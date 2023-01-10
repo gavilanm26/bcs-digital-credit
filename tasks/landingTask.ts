c72c7c1cf20ceaa4a1ed758f73b9e009a90d81ff
Mauricio Gavilan <57083001+gavilanm26@users.noreply.github.com>
2023-01-10 18:07:58 -0500

    Delete landingTask.ts

--- tasks/landingTask.ts
+++
@@ -1,7 +0,0 @@
-import { expect, Locator } from "@playwright/test";
-
-export class LandingTask {
-    async landingVerify(txtComoSolicitarlo: Locator) {
-        await expect(txtComoSolicitarlo).toContainText('Tiene un crédito preaprobado de libre inversión');
-    }
-}
